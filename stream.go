8347714e7ac1ffe8ea7bc8ddcaf23c3f9989bf14
Casper Mogensen <casper.mogensen@gmail.com>
2021-02-26 10:47:20 +0100

    Add stream

---
+++ stream.go
@@ -0,0 +1,38 @@
+package phoenix
+
+import (
+	"encoding/json"
+	"time"
+)
+
+type Stream struct {
+	Id         uint64      `db:"id" json:"id,omitempty" table:"device_streams"`
+	DeviceId   uint64      `db:"device_id" json:"device_id,omitempty"`
+	DeviceGuid *string     `json:"device_guid,omitempty"`
+	Code       string      `db:"code" json:"code"`
+	Timestamp  *time.Time  `db:"timestamp" json:"timestamp,omitempty"`
+	Value      interface{} `db:"value" json:"value"`
+}
+
+func (s *Stream) Notification() DeviceNotification {
+
+	raw_stream, err := json.Marshal(*s)
+	if err != nil {
+		panic(err)
+	}
+
+	n := DeviceNotification{
+		Notification: "stream",
+		Timestamp:    time.Now(),
+		Parameters:   json.RawMessage(raw_stream),
+	}
+
+	return n
+}
+
+type StreamCriteria struct {
+	DeviceId uint64 `schema:"device_id"`
+	Code     string `schema:"code"`
+
+	Limit int `schema:"limit"`
+}
