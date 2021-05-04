c4629e669312a86ff82f60f791b83d1bc6388027
Casper Mogensen <casper.mogensen@gmail.com>
2021-05-04 09:34:45 +0200

    Ignore empty values when encoding with schema

--- sample.go
+++ sample.go
@@ -28,13 +28,13 @@ type Sample struct {
 }
 
 type SampleCriteria struct {
-	Streams     []string  `schema:"stream" db:"stream"`
-	From        time.Time `schema:"from" db:"from"`
-	To          time.Time `schema:"to" db:"to"`
-	Frequency   string    `schema:"frequency" db:"frequency"`
-	IncludeDiff bool      `schema:"include_diff"`
+	Streams     []string   `schema:"stream,omitempty" db:"stream"`
+	From        *time.Time `schema:"from,omitempty" db:"from"`
+	To          *time.Time `schema:"to,omitempty" db:"to"`
+	Frequency   string     `schema:"frequency,omitempty" db:"frequency"`
+	IncludeDiff bool       `schema:"include_diff,omitempty"`
 
-	Limit int `schema:"limit"`
+	Limit int `schema:"limit,omitempty"`
 }
 
 type AverageConfig struct {
