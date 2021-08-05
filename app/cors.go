1ca8ff97022b8183dee95f6d8a64a6e2e105ba4e
Casper Mogensen <casper.mogensen@gmail.com>
2021-08-05 14:11:50 +0200

    Send CORS headers for all requests

--- app/cors.go
+++ app/cors.go
@@ -8,10 +8,11 @@ import (
 
 func Cors() negroni.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request, next http.HandlerFunc) {
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
+		w.Header().Set("Access-Control-Allow-Headers", "Accept, Accept-Language, Content-Type")
+
 		if r.Method == "OPTIONS" {
-			w.Header().Set("Access-Control-Allow-Origin", "*")
-			w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
-			w.Header().Set("Access-Control-Allow-Headers", "Accept, Accept-Language, Content-Type")
 			w.WriteHeader(http.StatusNoContent)
 			return
 		}
