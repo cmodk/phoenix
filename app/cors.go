54c0605c4ea702fbf0979a3739129ae58090e01c
Casper Mogensen <casper.mogensen@gmail.com>
2021-08-05 13:31:24 +0200

    Allow CORS for all domains

--- app/cors.go
+++ app/cors.go
@@ -8,13 +8,11 @@ import (
 
 func Cors() negroni.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request, next http.HandlerFunc) {
-		if origin := r.Header.Get("Origin"); origin != "" {
-			w.Header().Set("Access-Control-Allow-Origin", origin)
+		if r.Method == "OPTIONS" {
+			w.Header().Set("Access-Control-Allow-Origin", "*")
 			w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
 			w.Header().Set("Access-Control-Allow-Headers", "Accept, Accept-Language, Content-Type")
-		}
-		// Stop here if its Preflighted OPTIONS request
-		if r.Method == "OPTIONS" {
+			w.WriteHeader(http.StatusNoContent)
 			return
 		}
 		next(w, r)
