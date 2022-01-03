84fe17823458b0834407acac2040cd4454ed7080
Casper Lyngesen Mogensen <casper.mogensen@gmail.com>
2022-01-03 12:06:41 +0100

    First shot at common database handling for differint entities

---
+++ app/db_entity.go
@@ -0,0 +1,56 @@
+package app
+
+import (
+	"fmt"
+)
+
+type DatabaseRepository struct {
+	Table    string
+	Database *Database
+}
+
+func (repo *DatabaseRepository) List(dst Entity, c Criteria) error {
+
+	err := repo.Database.Match(dst, repo.Table, c)
+
+	if err != nil {
+		return err
+	}
+
+	if HasPopulate(c) {
+		dst.Populate()
+	}
+
+	return nil
+
+}
+
+func (repo *DatabaseRepository) Get(dst Entity, c Criteria) error {
+	err := repo.Database.MatchOne(dst, repo.Table, c)
+
+	if err != nil {
+		return err
+	}
+
+	if HasPopulate(c) {
+		dst.Populate()
+	}
+
+	return nil
+
+}
+
+func (repo *DatabaseRepository) Create(dst Entity) error {
+	err := repo.Database.Insert(dst, repo.Table)
+
+	if err != nil {
+		return err
+	}
+
+	return nil
+
+}
+
+type Entity interface {
+	Populate() error
+}
