a5fb22e30351ca40763d14b8c63df842a5136da0
Casper Mogensen <casper.mogensen@gmail.com>
2022-01-10 15:34:38 +0100

    Make build_applications aware of different os'es and arches

--- build_applications.sh
+++ build_applications.sh
@@ -1,2 +1,18 @@
 #!/bin/bash
-go build -o bin ./cmd/*
+GOOS=linux
+GOARCH=amd64
+
+if [ ! -z "$1" ]; then
+  GOOS=$1
+fi
+
+if [ ! -z "$2" ]; then
+  GOARCH=$2
+fi
+
+OUTDIR=bin/$GOOS/$GOARCH
+export GO111MODULE=off
+export GOOS=$GOOS
+export GOARCH=$GOARCH
+mkdir -p $OUTDIR
+go build -o $OUTDIR ./cmd/*
