eb088933442f3a03af950ae23a9305f1f9b74e23
Casper Mogensen <casper.mogensen@gmail.com>
2025-01-02 18:58:01 +0100

    Update database structure

--- database_structure.go
+++ database_structure.go
@@ -3,7 +3,6 @@ package phoenix
 var (
 	DatabaseStructure = []string{
 		"INVALID SQL, index 0 is not allowed for database updated",
-
 		"CREATE TABLE `device_commands`(`id` bigint(20) UNSIGNED NOT NULL,`device_id` bigint(20) UNSIGNED NOT NULL,`device_guid` varchar(256) NOT NULL, `command` varchar(256) NOT NULL, `created` timestamp NOT NULL DEFAULT current_timestamp(), `parameters` blob DEFAULT NULL) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;",
 		"ALTER TABLE `device_commands`  ADD UNIQUE KEY `id` (`id`), ADD KEY `device_id` (`device_id`), ADD KEY `device_guid` (`device_guid`);",
 		"ALTER TABLE `device_commands` MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;",
@@ -12,5 +11,7 @@ var (
 		"ALTER TABLE `device_commands` ADD CONSTRAINT `device_commands_device_guid_lock` FOREIGN KEY (`device_guid`) REFERENCES `devices` (`guid`);",
 		"ALTER TABLE `device_commands` ADD `pending` TINYINT NOT NULL AFTER `parameters`;",
 		"ALTER TABLE `devices` ADD `token_expiration` TIMESTAMP NULL AFTER `token`;",
+		"ALTER TABLE `device_commands` ADD `response` BLOB NULL AFTER `pending`;",
+		"ALTER TABLE `devices` ADD `low_memory_device` BOOLEAN NOT NULL AFTER `online`;",
 	}
 )
